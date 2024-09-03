ecf8839d717e5fc4f6de6382deb0df18fad1bcb1
gchamb <gianttechambers@gmail.com>
2024-09-03 11:59:50 -0500

    leaving comments for myself

--- egoist-functions/src/functions/monthly.ts
+++ egoist-functions/src/functions/monthly.ts
@@ -1,10 +1,18 @@
 import { app, InvocationContext, Timer } from "@azure/functions";
 
-export async function monthly(myTimer: Timer, context: InvocationContext): Promise<void> {
-    context.log('Timer function processed request.');
+export async function monthly(
+  myTimer: Timer,
+  context: InvocationContext
+): Promise<void> {
+  context.log("Timer function processed request.");
+  // query all the users that don't have an active revenue cat subscriber row
+
+  // send event to queue with the following data
+  // { uid:string, frequency: monthly, month: current_month - 1}
 }
 
-app.timer('monthly', {
-    schedule: '0 0 5 1 * *',
-    handler: monthly
+app.timer("monthly", {
+  // start of every month at 5:00
+  schedule: "0 0 5 1 * *",
+  handler: monthly,
 });
